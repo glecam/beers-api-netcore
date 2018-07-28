50feb2a0ce0e8de2c91057e87a5be5feb321f1a9
Glenn Cambre <glecam@bitconsult.be>
2018-07-28 11:15:25 +0200

    Add exception handler middleware

---
+++ Beers.API/Middleware/ExceptionHandlerMiddleware.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Net;
+using System.Threading.Tasks;
+using Beers.API.Exceptions;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+
+namespace Beers.API.Middleware
+{
+    public class ExceptionHandlerMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public ExceptionHandlerMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+                await HandleExceptionAsync(context, ex);
+            }
+        }
+
+        private static Task HandleExceptionAsync(HttpContext context, Exception exception)
+        {
+            var code = HttpStatusCode.BadRequest; // 400 if unexpected
+
+            if (exception is DllNotFoundException || exception is NotFoundException) code = HttpStatusCode.NotFound;
+
+            var hostingEnvironment = (IHostingEnvironment)context.RequestServices.GetService(typeof(IHostingEnvironment));
+
+            var response = hostingEnvironment.IsDevelopment() 
+                ? JsonConvert.SerializeObject(new { error = exception.Message, stackTrace = exception.StackTrace, innerException = exception.InnerException?.ToString() }) 
+                : JsonConvert.SerializeObject(new { error = exception.Message });
+
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = (int)code;
+            return context.Response.WriteAsync(response);
+        }
+    }
+}
--- Beers.API/Models/Brewery.cs
+++ Beers.API/Models/Brewery.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections.Generic;
-
+﻿
 namespace Beers.API.Models
 {
     public class Brewery
